593c43960bf30d8821ddbe432aeff26a8589f2c7
Erika <pierreerika@MacBook-Pro-de-Erika.local>
2024-07-27 01:25:24 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+//appel de la config .dotenv
+dotenv.config();
+
+//gestion des routes et des requêtes HTTP.
+const app = express();
+
+//creation de la BDD
+const port = process.env.PORT;
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log(`📁 DATABASE CONNECTED 🐳`);
+}
+
+//permet d'ouvrir l'API à des requêtes provenant de domaines différents.
+app.use(cors());
+
+//route test du port
+app.get("/", (req, res) => {
+  res.send("Welcome on Vinyle Corner API");
+});
+
+//ecoute du port
+app.listen(port, () =>
+  console.log(`Server is listening on port : http://localhost:${port}`)
+);
